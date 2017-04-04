c8c9172ab8c4ddb56df214fcb1d5478142bf63a8
Garth Davy <davychd@yahoo.com>
2017-04-04 04:37:31 +0000

    initial commit

--- .~c9_invoke_pdpQ3g.py
+++ .~c9_invoke_pdpQ3g.py
@@ -20,7 +20,7 @@ if thumbnail_spec and thumbnail_spec['href']:
     print ''
 
 
-image = %s"
+image = """<img src="%s"><br />"""
 for img in soup.findAll("img", src=True):
    print image % urlparse.urljoin(url, img["src"])
-   print print img.get("src")
+   print ''
